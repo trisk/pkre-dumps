cd7a4d6083c6a7fe2c21cdcc42c746ac300f2b62
Albert Lee <trisk@forkgnu.org>
2016-08-14 19:20:46 +0000

    Add decoded request signature

---
+++ request-logs-example-0.33.0/1470696993365.signature.js
@@ -0,0 +1,486 @@
+timestamp_since_start: 367840
+location_fix {
+  provider: "network"
+  timestamp_snapshot: 150661
+  latitude: 39.189003
+  longitude: -96.586006
+  altitude: 19.434
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 151782
+  latitude: 360
+  longitude: -360
+  altitude: 24
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 69883
+  latitude: 360
+  longitude: -360
+  altitude: 24
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "network"
+  timestamp_snapshot: 78292
+  latitude: 360
+  longitude: -360
+  altitude: 19.454
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "network"
+  timestamp_snapshot: 328440
+  latitude: 360
+  longitude: -360
+  altitude: 19.412
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "network"
+  timestamp_snapshot: 337469
+  latitude: 39.189003
+  longitude: -96.586006
+  altitude: 19.452
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 341778
+  latitude: 360
+  longitude: -360
+  altitude: 8
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "network"
+  timestamp_snapshot: 342470
+  latitude: 360
+  longitude: -360
+  altitude: 19.451
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 342780
+  latitude: 360
+  longitude: -360.00003
+  altitude: 7
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 343777
+  latitude: 360
+  longitude: -360
+  altitude: 5
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 344779
+  latitude: 39.189003
+  longitude: -96.586006
+  altitude: 18
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 345932
+  latitude: 360
+  longitude: -360
+  altitude: 11
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 346951
+  latitude: 360
+  longitude: -360
+  altitude: 16
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 347923
+  latitude: 360
+  longitude: -360
+  altitude: 18
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 348894
+  latitude: 360
+  longitude: -360
+  altitude: 16
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 349877
+  latitude: 39.189003
+  longitude: -96.586006
+  altitude: 15
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 350903
+  latitude: 360
+  longitude: -360
+  altitude: 13
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 351905
+  latitude: 360
+  longitude: -360
+  altitude: 14
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 352880
+  latitude: 360
+  longitude: -360
+  altitude: 15
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 353889
+  latitude: 360
+  longitude: -360
+  altitude: 13
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 354896
+  latitude: 39.189003
+  longitude: -96.586006
+  altitude: 14
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 355909
+  latitude: 360
+  longitude: -360
+  altitude: 12
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 356914
+  latitude: 360
+  longitude: -360
+  altitude: 11
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 357931
+  latitude: 360
+  longitude: -360
+  altitude: 11
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 358878
+  latitude: 360
+  longitude: -360
+  altitude: 9
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 359934
+  latitude: 39.189003
+  longitude: -96.586006
+  altitude: 7
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 360927
+  latitude: 360
+  longitude: -360
+  altitude: 7
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 361914
+  latitude: 360
+  longitude: -360
+  altitude: 6
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 362914
+  latitude: 360
+  longitude: -360
+  altitude: 5
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 363921
+  latitude: 360
+  longitude: -360
+  altitude: 5
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 364917
+  latitude: 39.189003
+  longitude: -96.586006
+  altitude: 4
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 365952
+  latitude: 39.188694
+  longitude: -96.585808
+  altitude: 5
+  provider_status: 3
+  location_type: 1
+}
+location_fix {
+  provider: "gps"
+  timestamp_snapshot: 366934
+  latitude: 39.188694
+  longitude: -96.585808
+  altitude: 5
+  provider_status: 3
+  location_type: 1
+}
+gps_info {
+  time_to_fix: 11223
+  satellites_prn: 2
+  satellites_prn: 5
+  satellites_prn: 12
+  satellites_prn: 13
+  satellites_prn: 15
+  satellites_prn: 18
+  satellites_prn: 20
+  satellites_prn: 21
+  satellites_prn: 25
+  satellites_prn: 26
+  satellites_prn: 29
+  satellites_prn: 68
+  satellites_prn: 69
+  satellites_prn: 70
+  satellites_prn: 71
+  satellites_prn: 75
+  satellites_prn: 83
+  satellites_prn: 84
+  satellites_prn: 85
+  satellites_prn: 86
+  snr: 77
+  snr: 50
+  snr: 196
+  snr: 130
+  snr: 167
+  snr: 227
+  snr: 229
+  snr: 282
+  snr: 232
+  snr: 326
+  snr: 310
+  snr: 28
+  snr: 39
+  snr: 182
+  snr: 201
+  snr: 261
+  snr: 119
+  snr: 92
+  snr: 343
+  snr: 313
+  azimuth: 34
+  azimuth: 65
+  azimuth: 14
+  azimuth: 27
+  azimuth: 18
+  azimuth: 3
+  azimuth: 54
+  azimuth: 19
+  azimuth: 30
+  azimuth: 3
+  azimuth: 67
+  azimuth: 2
+  azimuth: 49
+  azimuth: 63
+  azimuth: 14
+  azimuth: 2
+  azimuth: 4
+  azimuth: 52
+  azimuth: 56
+  azimuth: 8
+  elevation: 22.6
+  elevation: 22.3
+  elevation: 16
+  elevation: 19.5
+  elevation: 0
+  elevation: 0
+  elevation: 18.4
+  elevation: 13.5
+  elevation: 21.2
+  elevation: 0
+  elevation: 24.1
+  elevation: 0
+  elevation: 10.1
+  elevation: 10.4
+  elevation: 0
+  elevation: 0
+  elevation: 0
+  elevation: 19.5
+  elevation: 24.2
+  elevation: 18.7
+  has_almanac: true
+  has_almanac: true
+  has_almanac: true
+  has_almanac: true
+  has_almanac: true
+  has_almanac: true
+  has_almanac: true
+  has_almanac: true
+  has_almanac: true
+  has_almanac: true
+  has_almanac: true
+  has_almanac: false
+  has_almanac: true
+  has_almanac: false
+  has_almanac: false
+  has_almanac: false
+  has_almanac: false
+  has_almanac: true
+  has_almanac: true
+  has_almanac: false
+  has_ephemeris: true
+  has_ephemeris: true
+  has_ephemeris: true
+  has_ephemeris: true
+  has_ephemeris: true
+  has_ephemeris: true
+  has_ephemeris: true
+  has_ephemeris: true
+  has_ephemeris: true
+  has_ephemeris: true
+  has_ephemeris: true
+  has_ephemeris: false
+  has_ephemeris: true
+  has_ephemeris: false
+  has_ephemeris: false
+  has_ephemeris: false
+  has_ephemeris: false
+  has_ephemeris: true
+  has_ephemeris: true
+  has_ephemeris: false
+  used_in_fix: true
+  used_in_fix: true
+  used_in_fix: true
+  used_in_fix: true
+  used_in_fix: false
+  used_in_fix: false
+  used_in_fix: true
+  used_in_fix: true
+  used_in_fix: true
+  used_in_fix: false
+  used_in_fix: true
+  used_in_fix: false
+  used_in_fix: true
+  used_in_fix: false
+  used_in_fix: false
+  used_in_fix: false
+  used_in_fix: false
+  used_in_fix: true
+  used_in_fix: true
+  used_in_fix: false
+}
+sensor_info {
+  timestamp_snapshot: 365423
+  magnetometer_x: -0.11522424221038818
+  magnetometer_y: 0.24285697937011719
+  magnetometer_z: -0.030381202697753906
+  angle_normalized_x: -5
+  angle_normalized_y: -45.5
+  angle_normalized_z: -20.75
+  accel_raw_x: 0.19949544966220856
+  accel_raw_y: -0.29007041454315186
+  accel_raw_z: -0.89628112316131592
+  gyroscope_raw_x: -0.12095131725072861
+  gyroscope_raw_y: -0.20402899384498596
+  gyroscope_raw_z: -0.047647487372159958
+  accel_normalized_x: -1.9725197553634644
+  accel_normalized_y: 6.1544499397277832
+  accel_normalized_z: 7.3757920265197754
+  accelerometer_axes: 3
+}
+device_info {
+  device_id: "ee4bdf9e2ea04751"
+  android_board_name: "MSM8960"
+  android_bootloader: "0x30BE"
+  device_brand: "motorola"
+  device_model: "ghost"
+  device_model_identifier: "LPAS23.12-21.7-1"
+  device_model_boot: "qcom"
+  hardware_manufacturer: "motorola"
+  hardware_model: "XT1058"
+  firmware_brand: "ghost_att"
+  firmware_tags: "release-keys"
+  firmware_type: "user"
+  firmware_fingerprint: "motorola/ghost_att/ghost:5.1/LPAS23.12-21.7-1/1:user/release-keys"
+}
+activity_status {
+}
+location_hash1: 2110894950
+location_hash2: 18446744072890709307
+session_hash: "BFw}\200k\314\237\326^\347\271H\377#\247"
+timestamp: 1470696993365
+request_hash: -6838744052415167613
+request_hash: 2330107074095620424
+request_hash: 633179015024592320
+request_hash: 8610687699460396378
+request_hash: 2290943642799940151
